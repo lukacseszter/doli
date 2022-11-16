2b27b169080b5b98f5a1626c9b0a9ffa0aacffa8
Lukács Eszter <lukacseszter.leo@gmail.com>
2022-11-16 17:05:25 +0100

    Kész

---
+++ src/app/emp/emp.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-emp',
+  templateUrl: './emp.component.html',
+  styleUrls: ['./emp.component.scss']
+})
+export class EmpComponent implements OnInit {
+  employees:any = []
+  constructor() { }
+
+  ngOnInit(): void {
+    this.employees = [
+      {id: 1, name: "Virág Béla", city: "Szeged"},
+      {id: 2, name: "Teknős Irén", city: "Szolnok"},
+      {id: 3, name: "Parlag Ferenc", city: "Pécs"}
+    ]
+  }
+
+}
